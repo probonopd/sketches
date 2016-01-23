97586bbcb32193584fad9d185f73de6455ffc019
probonopd <probonopd@users.noreply.github.com>
2016-01-23 18:40:10 +0100

    Create credentials.h

---
+++ esp8266-mqtt-neopixel-ota/credentials.h
@@ -0,0 +1,11 @@
+/************************* WiFi Access Point *********************************/
+
+#define WLAN_SSID       "..."
+#define WLAN_PASS       "..."
+
+/************************* Adafruit.io Setup *********************************/
+
+#define AIO_SERVER      "io.adafruit.com"
+#define AIO_SERVERPORT  61883
+#define AIO_USERNAME    "..."
+#define AIO_KEY         "..."
